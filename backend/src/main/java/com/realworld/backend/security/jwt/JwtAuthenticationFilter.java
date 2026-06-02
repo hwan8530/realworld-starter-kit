f35c124a41b2a22f00f261f98cc6c7b0b5da7cd7
hwan8530 <aass2356@gmail.com>
2026-06-02 15:45:57 +0900

    TODO: extract login object from body

--- backend/src/main/java/com/realworld/backend/security/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/realworld/backend/security/jwt/JwtAuthenticationFilter.java
@@ -7,12 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
 @RequiredArgsConstructor
@@ -26,7 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       FilterChain filterChain) throws ServletException, IOException {
     String token = extractToken(request);
     if (token != null && jwtUtil.validateOnlyToken(token)) {
-      Authentication authentication = jwtUtil.getAuthentication(token);
+      Authentication authentication = jwtUtil.getAuthentication(token, null);
       if (authentication != null) {
         // SecurityContext에 인증 정보 저장
         SecurityContextHolder.getContext().setAuthentication(authentication);
